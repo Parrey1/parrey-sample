b2dd7638c03368667bd52bb6b1c6a9d0ca5c8a84
parrey <precious.Arrey@cognizant.com>
2023-03-06 14:04:10 -0600

    tffiles

---
+++ main.tf
@@ -0,0 +1,53 @@
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "tfsample"
+  }
+}
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "tfsample"
+  }
+}
+resource "aws_network_interface" "foo" {
+  subnet_id   = aws_subnet.main.id
+  private_ips = ["10.0.1.100"]
+
+  tags = {
+    Name = "sample_network_interface"
+  }
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+
+  network_interface {
+    network_interface_id = aws_network_interface.foo.id
+    device_index         = 0
+  }
+ }
